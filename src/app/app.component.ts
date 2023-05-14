c2c44cf1989f17a6d3ed1f4754dda4cbf30a4486
sriki <srikanthgowda2163@gmail.com>
2023-05-14 23:44:56 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, addDoc, collectionData, doc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ang-fire';
+  userData!: Observable<any>;
+
+  constructor(private firestore: Firestore) {
+    this.getData();
+  }
+
+  addData(f: any) {
+    const collectionInstance = collection(this.firestore, 'users');
+    console.log(f.value);
+
+    addDoc(collectionInstance, f.value)
+      .then(() => {
+        console.log("Data added success");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getData() {
+    const collectionInstance = collection(this.firestore, 'users');
+    collectionData(collectionInstance, {idField:'id'})
+      .subscribe(val =>{
+        console.log(val);
+      })
+
+      this.userData=collectionData(collectionInstance,{idField:'id'});
+  }
+
+  updateData(id:string){
+    const docInstance=doc(this.firestore,'users',id);
+    const updateData={
+      name:'updateName',
+      email:'updateEmail'
+    }
+      updateDoc(docInstance,updateData)
+      .then(()=>{
+        console.log('Data Updated');
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+
+  }
+
+  deleteData(id:string){
+    const docInstance=doc(this.firestore,'users', id);
+    deleteDoc(docInstance)
+    .then(()=>{
+      console.log('Data Deleted Successfully');
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'ang-fire-35b5e',
+    appId: '1:849103143117:web:5757c5fda6a19b29586984',
+    storageBucket: 'ang-fire-35b5e.appspot.com',
+    apiKey: 'AIzaSyCW4TxMuDrWB0edWq3N-lq3_rMTzlMLCqs',
+    authDomain: 'ang-fire-35b5e.firebaseapp.com',
+    messagingSenderId: '849103143117',
+  },
+    production:false
+}
